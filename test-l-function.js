7a94132e5f29bba988a98c5e06da04a43e407ed3
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2026-01-14 17:19:16 +0100

    parser

--- test-l-function.js
+++ test-l-function.js
@@ -5,3 +5,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const { data, error } = await resend.emails.receiving.list();
 console.log("Liste des emails reçus:", data);
+data.data.map(async(mail)=>{
+  
+
+const { data:content, error } = await resend.emails.receiving.get(mail.id);
+console.log(content)
+})
